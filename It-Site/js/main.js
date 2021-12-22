55fb1c77a68346713404c0b4e82e8ea89844f041
Nelson Guerra <guerranelson1030@gmail.com>
2021-12-21 19:08:32 -0500

    init

---
+++ It-Site/js/main.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.contact', { delay: 500, reset: true });
+ScrollReveal().reveal('.about', { delay: 500, reset: true });
+ScrollReveal().reveal('.resources', { delay: 500, reset: true });
+ScrollReveal().reveal('.projects', { delay: 500, reset: true });
+
+//section Home
+
+//setting colors
+const mainsColor = ['#008fbb', '#00a2ee', '#f0fbff', '#e6f4f1', '#EF972C'];
